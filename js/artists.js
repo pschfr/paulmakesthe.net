30f21a1df88d7573b61be36f72b1496a1c893822
Paul Schaefer <yeppaul2@gmail.com>
2017-07-28 15:30:46 -0400

    Added artists to about page, added mailer PHP, design tweaks'

---
+++ js/artists.js
@@ -0,0 +1,33 @@
+function lastFM_request() {
+	var lastFMurl = 'https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=paul_r_schaefer&api_key=0f680404e39c821cac34008cc4d803db&limit=10&format=json',
+		xhr = new XMLHttpRequest(),
+		element = document.getElementById('artists');
+
+	xhr.open('GET', lastFMurl, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var obj = JSON.parse(xhr.responseText),
+				artists = obj.topartists.artist;
+
+			// Empty element
+			element.innerHTML = '';
+
+			// Loop over artists, appending to page
+			for (var i = 0; i < artists.length; i++) {
+				var linkText = '<a href="' + artists[i].url + '" target="_blank" rel="noreferrer noopener" class="grow link dib tc w-20-ns w-50 pa2 mb4">',
+					imgText = '<img src="' + artists[i].image[3]['\#text'] + '" alt="' + artists[i].name + '" /><br/>',
+					artistText = '<strong>' + artists[i].name + '</strong><br/>',
+					artistPlays = '<small>' + parseInt(artists[i].playcount).toLocaleString() + ' plays</small>'
+					linkEnd = '</a>';
+				element.innerHTML += linkText + imgText + artistText + artistPlays + linkEnd;
+			}
+
+		} else {
+			element.innerHTML = 'Unfortunately, the connection to Last.fm failed.';
+		}
+	};
+	xhr.send(null); // Close connection
+}
+
+// Run on load
+lastFM_request();
---
+++ php/mailer.php
@@ -0,0 +1,17 @@
+<?php
+	if(!empty($_POST['name']) && !empty($_POST['email'])) {
+		$to = "yeppaul2@gmail.com";
+		$subject = "Message from your website!";
+		$header  = "Reply-To: <" . $_POST['email'] . ">\n";
+		$header .= "X-Mailer: PHP/" . phpversion() . "\n";
+		$header .= "X-Priority: 1";
+		$body .= $_POST['message'];
+		if(@mail($to, $subject, $body, $header)) {
+			header('Location: /contact?thanks');
+		} else {
+			header('Location: /contact?error');
+		}
+	} else {
+		header('Location: /contact?error');
+	}
+?>
