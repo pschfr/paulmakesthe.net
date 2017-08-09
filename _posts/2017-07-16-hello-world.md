79f0cc948aa3edd87ff9f41b724b07b5df321fac
Paul Schaefer <yeppaul2@gmail.com>
2017-08-09 15:57:53 -0400

    added code highlighting, fixed live site URLs, added hello world post, fixed bugs throughout

---
+++ _posts/2017-07-16-hello-world.md
@@ -0,0 +1,23 @@
+---
+layout: post
+title: "Hello, World!"
+date: 2017-07-16 15:46:00 -0400
+categories: blog
+---
+One of the first things a programmer does when learning a new language is figure out how to print text on the screen. Most of the time, it's the famous "Hello, World!" text.
+
+&nbsp;
+
+```c
+#include <stdio.h>
+
+main()
+{
+	printf("Hello, World!\n");
+}
+```
+<small>*This is from a 1974 Bell Laboratories book, see the [Wikipedia article](https://en.wikipedia.org/wiki/%22Hello,_World!%22_program).*</small>
+
+&nbsp;
+
+So this is my hello to you. I hope you'll stick around.
--- php/mailer.php
+++ php/mailer.php
@@ -1,17 +1,17 @@
 <?php
 	if(!empty($_POST['name']) && !empty($_POST['email'])) {
-		$to = "yeppaul2@gmail.com";
+		$to = "hello@paulmakesthe.net";
 		$subject = "Message from your website!";
 		$header  = "Reply-To: <" . $_POST['email'] . ">\n";
 		$header .= "X-Mailer: PHP/" . phpversion() . "\n";
 		$header .= "X-Priority: 1";
 		$body .= $_POST['message'];
 		if(@mail($to, $subject, $body, $header)) {
-			header('Location: /contact?thanks');
+			header('Location: /contact.html?thanks');
 		} else {
-			header('Location: /contact?error');
+			header('Location: /contact.html?error');
 		}
 	} else {
-		header('Location: /contact?error');
+		header('Location: /contact.html?error');
 	}
 ?>
