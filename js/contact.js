22d4576682d4c2a8b47345a0aa9cbf701b1e52de
Paul Schaefer <yeppaul2@gmail.com>
2018-06-08 16:57:58 -0400

    Fixed bug in contact form thanks/error messages

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,12 @@
+// Hides map and form
+if (window.location.search.replace('?', '') == 'thanks' || window.location.search.replace('?', '') == 'error') {
+	document.getElementsByTagName('form')[0].style.display = 'none';
+	document.getElementById('map').style.display = 'none';
+}
+
 // Show thanks or error messages
-	if (window.location.search.replace('?', '') == 'thanks') {
-		document.getElementsByTagName('form')[0].style.display = 'none'
-		document.getElementsByTagName('form')[0].parentElement.appendChild(document.createElement('p')).innerHTML = 'Thanks for your message! I\'ll get back to you right away.<br/><br/>Go back <a href="/">home</a>, or read some <a href="/archive">articles</a>.'
-	} else if (window.location.search.replace('?', '') == 'error') {
-		document.getElementsByTagName('form')[0].parentElement.appendChild(document.createElement('p')).innerHTML = 'It seems something went wrong. Please try sending your message again.'
-	}
+if (window.location.search.replace('?', '') == 'thanks') {
+	document.getElementsByTagName('form')[0].parentElement.appendChild(document.createElement('p')).innerHTML = 'Thanks for your message! I\'ll get back to you right away.<br/><br/>Go back <a href="/">home</a>, or read some <a href="/archive">articles</a>.';
+} else if (window.location.search.replace('?', '') == 'error') {
+	document.getElementsByTagName('form')[0].parentElement.appendChild(document.createElement('p')).innerHTML = 'It seems something went wrong. Please try sending your message again.<br/><br/><a href="javascript:history.back();">Try again.</a>';
+}
